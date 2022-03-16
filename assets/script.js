2783d7fa44793cc08806e403c38f90c1eb141e51
nickporjonathan <nickporjonathan@gmail.com>
2022-03-16 14:12:50 -0400

    revised load entity variable

--- assets/script.js
+++ assets/script.js
@@ -1,54 +1,56 @@
 var taskIDs = [
-    "0900",
-    "1000",
-    "1100",
-    "1200",
-    "1300",
-    "1400",
-    "1500",
-    "1600",
-    "1700",
+  "0900",
+  "1000",
+  "1100",
+  "1200",
+  "1300",
+  "1400",
+  "1500",
+  "1600",
+  "1700",
 ];
 
-$(".saveBtn").on("click", function(){
-    var taskElement = $(this).closest(".time-block").find(".taskbox");
-    var textInput = taskElement.val();
-    var taskId = taskElement.attr("id");
-    $(taskElement).attr("value", textInput);
-    localStorage.setItem(taskId, textInput);
+$(".saveBtn").on("click", function () {
+  var taskElement = $(this).closest(".time-block").find(".taskbox");
+  var textInput = taskElement.val();
+  var taskId = taskElement.attr("id");
+  $(taskElement).attr("value", textInput);
+  localStorage.setItem(taskId, textInput);
 });
 
-var loadEntity = function(){
-    taskIDs.forEach(function (element){
-        $(`#${element}`).val(localStorage.getItem)(element));
-    });
+var loadEntity = function () {
+  taskIDs.forEach(function (element) {
+    $(`#${element}`).val(localStorage.getItem(element));
+  });
 };
 
-var timeCheck = function(timeElement){
-    var hour = parseInt(
-        $(timeElement).find(".hour").attr("id").replace("hr")
-    );
-    var time = moment().hour();
-    if(hour<time){
-        $(timeElement).removeClass("present future");
-        $(timeElement).addClass("past");
-    }
-    else if(hour===time){
-        $(timeElement).removeClass("past future");
-        $(timeElement).addClass("present");
-    }
-    else{
-        $(timeElement).removeClass("present past");
-        $(timeElement).addClass("future");
-    }
+var timeCheck = function (timeElement) {
+  var hour = parseInt($(timeElement).find(".hour").attr("id").replace("hr"));
+  var time = moment().hour();
+  if (hour < time) {
+    $(timeElement).removeClass("present future");
+    $(timeElement).addClass("past");
+  } else if (hour === time) {
+    $(timeElement).removeClass("past future");
+    $(timeElement).addClass("present");
+  } else {
+    $(timeElement).removeClass("present past");
+    $(timeElement).addClass("future");
+  }
 };
 
-$(document).on("click", function(){
-    loadEntity();
+$(document).on("click", function () {
+  loadEntity();
 });
 
-$(document).ready(function(){
-    var date = moment().format("YYYY-MM-DD hh:mm:ss");
-    $("#currentDay").text(date);
+$(document).ready(function () {
+  var date = moment().format("YYYY-MM-DD hh:mm:ss");
+  $("#currentDay").text(date);
 });
 
+loadEntity();
+$(".time-block").each(function (index, element) {
+  timeCheck(element);
+});
+
+setInterval(checkTime(), 1000 * 60 * 2);
